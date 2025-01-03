d657820f3cbd7cb1c57a4ea3552c575a0de60e1d
le ke hien <killer0121906@gmail.com>
2025-01-04 05:19:16 +0700

    -- fix cors

--- media-scraper-backend/src/app.js
+++ media-scraper-backend/src/app.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors({
     origin: ['http://localhost:3000',
-    'https://lekehien-mediascraper.onrender.com',]
+    'https://lekehien-mediascraper.onrender.com']
   }));
 app.use(express.json())
 
