51965968fe267c4a1916c1002e8c05890002d573
le ke hien <killer0121906@gmail.com>
2025-01-04 05:57:53 +0700

    -- fix  axios

--- media-scraper-frontend/src/config/axiosInstance.js
+++ media-scraper-frontend/src/config/axiosInstance.js
@@ -6,10 +6,10 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001', 
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/x-www-form-urlencoded",
     'Access-Control-Allow-Origin': '*', // Allow all origins or specify the domain if needed
   },
-  withCredentials: true, 
+  withCredentials: false, 
 });
 
 
