f6facb3334c54a9c9951837a05615f2d171a359b
le ke hien <killer0121906@gmail.com>
2025-01-08 21:01:22 +0700

    -- fix database

--- media-scraper-backend/src/config/database.js
+++ media-scraper-backend/src/config/database.js
@@ -1,19 +1,17 @@
 const { Sequelize } = require('sequelize');
 const config = require('../config/config.json')
 const fs = require('fs');
+const sslCaContent = fs.readFileSync(process.env.SSL_CA_PATH).toString();
 const sequelize = new Sequelize(process.env.databaseURL || config.development.url, {
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
-      ca: fs.readFileSync(process.env.SSL_CA_PATH).toString(),
+      ca: sslCaContent,
       rejectUnauthorized: true,  // Ensure that the certificate is validated
     },
   },
   logging: console.log,
 });
-console.log("asdfdas   :",   process.env.SSL_CA_PATH);
-const sslCaContent = fs.readFileSync(process.env.SSL_CA_PATH).toString();
-console.log(" data 1  2 43 :",sslCaContent)
 sequelize.sync({ alter: true })
   .then(() => {
     console.log('Database synced successfully');
