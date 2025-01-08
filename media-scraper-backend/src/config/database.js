0fa0cad24624147e6115f3205dd4e4c3aa5e838f
le ke hien <killer0121906@gmail.com>
2025-01-08 13:53:05 +0700

    -- fix databse  js

--- media-scraper-backend/src/config/database.js
+++ media-scraper-backend/src/config/database.js
@@ -1,12 +1,15 @@
 const { Sequelize } = require('sequelize');
 const config = require('../config/config.json')
 
-const sequelize = new Sequelize(process.env.databaseURL||config.development.url, {
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: false,
+const sequelize = new Sequelize(process.env.databaseURL || config.development.url, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      ca: fs.readFileSync(process.env.SSL_CA_PATH).toString(),
+      rejectUnauthorized: true,  // Ensure that the certificate is validated
+    },
   },
-  logging: console.log, 
+  logging: console.log,
 });
 
 sequelize.sync({ alter: true })
